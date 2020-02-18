4900c78866ff5ce5e5cc1a1c484abf6272c6e404
utahack <kou.00033.3@gmail.com>
2020-02-19 03:09:31 +0900

    [fix] footer item css

--- create_sw.py
+++ create_sw.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os.path
+import re
+from urllib.parse import urljoin, urlparse
 import json
 from pathlib import Path
 
 suffixs = {'.js', '.png', '.html', '.jpg', '.ico', '.json', '.css', '.gif'}
 ignores = {'test', '.git', '.vscode', 'service_worker.js'}
+
+def linkiter(html):
+    for m in re.finditer(r'(href|src)="(.*?)"', html):
+        url = urlparse(urljoin('https://entm.auone.jp', m.group(2)))
+        ext = os.path.splitext(url.geturl())[1]
+        yield url.geturl()
+
 
 def allow(p):
     for ignore in ignores:
@@ -13,9 +23,22 @@ def allow(p):
             return False
     return p.suffix in suffixs
 
-template = Path('service_worker.tpl').read_text(encoding='utf-8')
-cacheURLs = [str(p).replace('\\', '/') for p in Path('.').glob('**/*.*') if allow(p)]
+def main():
+    template = Path('service_worker.tpl').read_text(encoding='utf-8')
+    cacheURLs = {str(p).replace('\\', '/') for p in Path('.').glob('**/*.*') if allow(p)}
+    cacheURLs.add('//www.googletagmanager.com/gtm.js?id=GTM-K25DZQH')
+    path = Path('.')
+    for p in path.glob('**/*.html'):
+        html = p.read_text(encoding='utf-8')
+        cacheURLs = cacheURLs or set(linkiter(html))
 
-template = template.replace('$cacheURLs', json.dumps(cacheURLs, indent=4))
+    template = template.replace('$cacheURLs', json.dumps(list(cacheURLs), indent=4))
+    Path('service_worker.js').write_text(template, encoding='utf-8')
 
-Path('service_worker.js').write_text(template, encoding='utf-8')
+
+if __name__ == '__main__':
+    main()
+
+
+
+
