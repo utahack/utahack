c2faa33324c33afe190c75d43cf340a5e69b7231
utahack <kou.00033.3@gmail.com>
2020-02-18 22:41:34 +0900

    fix

---
+++ js/wrapper.js
@@ -0,0 +1,40 @@
+const mainFrame = document.getElementById('mainFrame');
+const titleHeader = document.getElementById('titleHeader');
+const backButton = document.getElementById('backButton');
+const topButton = document.getElementById('topButton');
+const resetButton = document.getElementById('resetButton');
+const loadBlock = document.getElementById('loadBlock');
+
+window.timeStamp = [0, 0, 0];
+
+window.startLoading = (ms=1000) => {
+  loadBlock.classList.add('loading');
+  window.stopLoading(ms);
+}
+
+window.stopLoading = (ms=0) => {
+  setTimeout(()=>loadBlock.classList.remove('loading'), ms);
+}
+
+mainFrame.addEventListener('load', e => {
+  const ifrm = mainFrame.contentWindow;
+  titleHeader.textContent = ifrm.document.title;
+  const script = document.createElement('script');
+  script.src = '/js/mod.js';
+  ifrm.document.body.appendChild(script);
+  ifrm.addEventListener('beforeunload', ()=>{
+    window.startLoading(1000);
+  }, false);
+}, false);
+
+backButton.addEventListener('click', () => {
+  mainFrame.contentWindow.history.back();
+}, false);
+
+topButton.addEventListener('click', () => {
+  mainFrame.src = "/aukaraokecp";
+}, false);
+
+resetButton.addEventListener('click', () => {
+  if (confirm('')) location.reload();
+}, false);
